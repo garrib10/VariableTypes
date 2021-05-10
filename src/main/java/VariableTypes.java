823a873bf1cb9324e73d51c237410ea725bb2aca
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2021-05-10 18:47:59 -0400

    2nd commit

---
+++ src/main/java/VariableTypes.java
@@ -0,0 +1,9 @@
+public class VariableTypes {
+    public static void main(String[] args) {
+     System.out.println( "String name = Cleopatra;");
+      System.out.println (" age = 2016 + 69;");
+     System.out.println (" double price = 1999.99;");
+     System.out.println (" boolean truth = true;");
+      System.out.println ("boolean falseness = !truth;");
+    }
+}
